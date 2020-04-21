5f44f229474b7083ae91237f5ed78ea1d652bd22
Your Name <you@example.com>
2020-04-21 19:48:56 +0800

    图形验证码

--- meiduo_mall/meiduo_mall/apps/vertifications/urls.py
+++ meiduo_mall/meiduo_mall/apps/vertifications/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import re_path
 from . import views
 
 urlpatterns = [
+    re_path(r'^image_codes/(?P<uuid>[\w-]+)/$', views.ImageCodeView.as_view()),
 
 ]
--- meiduo_mall/meiduo_mall/apps/vertifications/views.py
+++ meiduo_mall/meiduo_mall/apps/vertifications/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+from meiduo_mall.libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
+
+class ImageCodeView(View):
+
+    def get(self, request, uuid):
+        '''生成图形验证码,保存到redis中, 返回图片
+        :param request:请求对象
+        :param uuid: 浏览器端生成的唯一id
+        return:一个图片
+        '''
+
+        #1. 调用工具类 captcha 生成图形验证码
+        text, image = captcha.generate_captcha()
+
+        #2. 链接redis,获取连接对象
+        redis_conn = get_redis_connection('verify_code')
+
+        #3.利用连接对象,保存数据到redis, 使用setex函数
+        #redis_conn.setex('<key>', '<expire>', '<value>')
+
+        redis_conn.setex('imag_%s' % uuid, 300, text)
+
+        #4. 返回图片
+
+        return HttpResponse(image,
+                            content_type='image/jpg')
