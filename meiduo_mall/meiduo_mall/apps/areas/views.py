c08ba4a2def759b1fbb7fb6b85ab973b4a82355c
Your Name <you@example.com>
2020-05-04 22:15:50 +0800

    django缓存

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -2,30 +2,40 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from .models import Area
+from django.core.cache import cache
 
 # Create your views here.
 class ProvinceView(View):
 
     def get(self, request):
         '''前端发送请求,获取省份数据'''
-        try:
-            #1. 查询省级数据
-            province_model_list = Area.objects.filter(parent__isnull=True)
 
-            #创建一个list
-            list = []
+        list = cache.get('province')
 
-            #2. 整理省级数据
-            for province in province_model_list:
-                list.append({
-                    'id': province.id,
-                    'name': province.name
-                })
-        except Exception as e:
+        if not list:
 
-            #如果报错,则返回错误原因
-            return JsonResponse({'code':400,
-                                 'errmsg':'获取省份数据出错'})
+            try:
+                #1. 查询省级数据
+                province_model_list = Area.objects.filter(parent__isnull=True)
+
+                #创建一个list
+                list = []
+
+                #2. 整理省级数据
+                for province in province_model_list:
+                    list.append({
+                        'id': province.id,
+                        'name': province.name
+                    })
+
+
+                cache.set('province', list, 3600)
+
+            except Exception as e:
+
+                #如果报错,则返回错误原因
+                return JsonResponse({'code':400,
+                                     'errmsg':'获取省份数据出错'})
 
         #3. 返回整理好的省级数据
         return JsonResponse({'code':0,
@@ -37,34 +47,40 @@ class SubAreaView(View):
     def get(self, request, pk):
         '''接收pk值,通过pk值,获取对应的对象,还要获取pk对应的下一级数据'''
 
-        try:
-            #1.根据pk获取对应的对象
-            province = Area.objects.get(id=pk)
+        dict = cache.get('sub_data_%s' % pk)
 
-            #2.获取pk对应的下一级所有的数据对象
-            sub_model_list = Area.objects.filter(parent=pk)
+        if not dict:
 
-            list = []
+            try:
+                #1.根据pk获取对应的对象
+                province = Area.objects.get(id=pk)
+
+                #2.获取pk对应的下一级所有的数据对象
+                sub_model_list = Area.objects.filter(parent=pk)
+
+                list = []
 
 
-            #3.遍历下一级所有的数据对象,获取每一个
-            for sub_model in sub_model_list:
-                #4.把每一个对象 ====> {} ====> []
-                list.append({
-                    'id': sub_model.id,
-                    'name': sub_model.name
-                })
+                #3.遍历下一级所有的数据对象,获取每一个
+                for sub_model in sub_model_list:
+                    #4.把每一个对象 ====> {} ====> []
+                    list.append({
+                        'id': sub_model.id,
+                        'name': sub_model.name
+                    })
 
-            #5. 再创建一个dict, 把dictionary整理好
-            dict = {
-                'id':province.id,
-                'name':province.name,
-                'subs':list
-            }
+                #5. 再创建一个dict, 把dictionary整理好
+                dict = {
+                    'id':province.id,
+                    'name':province.name,
+                    'subs':list
+                }
 
-        except Exception as e:
-            return JsonResponse({'code':400,
-                                 'errmsg':'获取不到数据'})
+                cache.set('sub_data' + pk, dict, 3600)
+
+            except Exception as e:
+                return JsonResponse({'code':400,
+                                     'errmsg':'获取不到数据'})
 
         #6. 整理json返回
 
